b8a2129c400d1c1653ccdeaa4967d353557a45f2
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-11-02 13:13:05 +0300

    Task2

---
+++ Tyuiu.NeupokoevSV.Sprint4.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.NeupokoevSV.Sprint4.Task2.V22.Lib;
+namespace Tyuiu.NeupokoevSV.Sprint4.Task2.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 2, 3 };
+            int res = ds.Calculate(array);
+            Assert.AreEqual(675, res);
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint4.Task2.V22/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.NeupokoevSV.Sprint4.Task2.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int len;
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] nums = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            nums[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        int res = ds.Calculate(nums);
+        Console.WriteLine("res");
+        Console.WriteLine(res);
+    }
+}
