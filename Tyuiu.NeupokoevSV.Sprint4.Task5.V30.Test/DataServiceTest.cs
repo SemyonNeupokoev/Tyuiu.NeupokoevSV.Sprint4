4c45caac9efdafdb8e75660d89e727e39f7bc02b
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-11-03 13:54:11 +0300

    Task5

---
+++ Tyuiu.NeupokoevSV.Sprint4.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.NeupokoevSV.Sprint4.Task5.V30.Lib;
+namespace Tyuiu.NeupokoevSV.Sprint4.Task5.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] data = new int[5, 5] { { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 } };
+            int res = ds.Calculate(data);
+            Assert.AreEqual(25, res);
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint4.Task5.V30/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.NeupokoevSV.Sprint4.Task5.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int len;
+        len = Convert.ToInt32(Console.ReadLine());
+        int[,] nums = new int[len, len];
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                nums[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        int res = ds.Calculate(nums);
+        Console.WriteLine("res");
+        Console.WriteLine(res);
+    }
+}
